1ecc5d826dd0155055d930551f9bf064f433603a
Will Usher <wusher@kth.se>
2024-06-06 15:30:35 +0200

    Write out metadata for each pdf

--- get_pdfs.py
+++ get_pdfs.py
@@ -2,7 +2,7 @@ import os
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup, SoupStrainer
 import requests
-
+from csv import DictWriter
 
 from logging import basicConfig, getLogger, INFO
 
@@ -18,15 +18,36 @@ folder_location = r'webscraping'
 if not os.path.exists(folder_location):
     os.mkdir(folder_location)
 
+metadata = []
+
 with open('publication_links.csv', 'r') as f:
     publication_links = f.readlines()
     for link in sorted(set(publication_links)):
         response = requests.get(urljoin(url, link))
         soup = BeautifulSoup(response.text, "html.parser")
         for link in soup.select("a[href$='.pdf']"):
+
+            href = str(link['href'])
+
             # Name the pdf files using the last portion of each link which are unique in this case
-            filename = os.path.join(folder_location, link['href'].split('/')[-1])
+            filename = os.path.join(folder_location, href.split('/')[-1])
             filename = filename.replace("%20", "_")
+
+            authors = soup.find("div", {"class": "views-field-field-author"})
+            publication_date = soup.find("div", {"class": "views-field-field-publication-date"})
+
+            metadata.append({
+                "filename": filename,
+                "url": urljoin(url, href),
+                "authors": authors.find("div", {"class": "field-content"}).text if authors else None,
+                "publication_date": publication_date.find("time").text if publication_date else None,
+                })
+
             logger.info(f"Downloading {filename}")
             with open(filename, 'wb') as f:
-                f.write(requests.get(urljoin(url, link['href'])).content)
+                f.write(requests.get(urljoin(url, href)).content)
+
+with open('metadata.csv', 'w') as f:
+    writer = DictWriter(f, fieldnames=["filename", "url", "authors", "publication_date"])
+    writer.writeheader()
+    writer.writerows(metadata)
