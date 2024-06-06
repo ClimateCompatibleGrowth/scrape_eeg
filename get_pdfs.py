61a998d8800a201e097c810d269db9bacc61b611
Will Usher <wusher@kth.se>
2024-06-06 16:20:06 +0200

    Add title and summary to metadata, use caching

--- get_pdfs.py
+++ get_pdfs.py
@@ -1,7 +1,7 @@
 import os
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup, SoupStrainer
-import requests
+import requests_cache
 from csv import DictWriter
 
 from logging import basicConfig, getLogger, INFO
@@ -11,6 +11,7 @@ logger = getLogger(__name__)
 
 url = "https://www.energyeconomicgrowth.org/www.energyeconomicgrowth.org/content/publications.html"
 
+session = requests_cache.CachedSession("cache", expire_after=30)
 strainer = SoupStrainer("a", href=True)
 
 # If there is no such folder, the script will create one automatically
@@ -22,8 +23,8 @@ metadata = []
 
 with open('publication_links.csv', 'r') as f:
     publication_links = f.readlines()
-    for link in sorted(set(publication_links)):
-        response = requests.get(urljoin(url, link))
+    for publication in sorted(set(publication_links)):
+        response = session.get(urljoin(url, publication, allow_fragments=True))
         soup = BeautifulSoup(response.text, "html.parser")
         for link in soup.select("a[href$='.pdf']"):
 
@@ -35,19 +36,33 @@ with open('publication_links.csv', 'r') as f:
 
             authors = soup.find("div", {"class": "views-field-field-author"})
             publication_date = soup.find("div", {"class": "views-field-field-publication-date"})
+            title = soup.find('div', {'class': 'field--name-node-title'})
+            abstract = soup.find('div', {'class': 'field--type-text-with-summary'})
 
             metadata.append({
                 "filename": filename,
                 "url": urljoin(url, href),
                 "authors": authors.find("div", {"class": "field-content"}).text if authors else None,
                 "publication_date": publication_date.find("time").text if publication_date else None,
+                "parent_url": urljoin(url, publication),
+                'title': title.find('h2').text.strip() if title else None,
+                'abstract': abstract.find('p').text.strip() if abstract else None
                 })
 
-            logger.info(f"Downloading {filename}")
             with open(filename, 'wb') as f:
-                f.write(requests.get(urljoin(url, href)).content)
+                response = session.get(urljoin(url, href))
+                if response.status_code == 200:
+                    logger.info(f"Downloading {filename} from {href} in {publication}.")
+                    f.write(response.content)
+                else:
+                    logger.error(f"Failed to download {filename} from {href} in {publication}.")
+                    # Hacky manual munge to get the correct URL
+                    response = session.get("https://www.energyeconomicgrowth.org/www.energyeconomicgrowth.org/" + href[5:])
+                    if response.status_code == 200:
+                        logger.info(f"Downloading {filename} from {href} in {publication}.")
+                        f.write(response.content)
 
 with open('metadata.csv', 'w') as f:
-    writer = DictWriter(f, fieldnames=["filename", "url", "authors", "publication_date"])
+    writer = DictWriter(f, fieldnames=["title", "abstract", "filename", "url", "authors", "publication_date", "parent_url"])
     writer.writeheader()
     writer.writerows(metadata)
--- readme.md
+++ readme.md
@@ -2,10 +2,28 @@
 
 Two scripts to:
 
-i) obtain the URLs of all PDFs on the EEG website
-ii) download all the PDFs to a local folder
+1. obtain the URLs of all PDFs on the EEG website
+2. download all the PDFs to a local folder
 
-Dependencies
+Install dependencies
 
-beautifulsoup4
-requests_cache
+    conda create -n python=3.12 requests requests_cache beautifulsoup4
+
+Now run the scripts::
+
+    python scrape.py
+
+Then::
+
+    python get_pdf.py
+
+
+You should see a folder `webscraping` containing all the PDF files.
+Then there's a log file `app.log` which should contain a bunch of debugging messages.
+Then `metadata.csv` which contains all of the details about the files scraped from the site including title, publication date, summary and authors.
+
+
+## Dependencies
+
+- beautifulsoup4
+- requests_cache
