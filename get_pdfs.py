8f26b9f525388505b3d8a3d6f6c86692f46ac5b2
Will Usher <wusher@kth.se>
2024-06-06 14:54:47 +0200

    Don't use cache and tidy filenames

--- get_pdfs.py
+++ get_pdfs.py
@@ -1,7 +1,8 @@
 import os
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup, SoupStrainer
-import requests_cache
+import requests
+
 
 from logging import basicConfig, getLogger, INFO
 
@@ -10,7 +11,6 @@ logger = getLogger(__name__)
 
 url = "https://www.energyeconomicgrowth.org/www.energyeconomicgrowth.org/content/publications.html"
 
-session = requests_cache.CachedSession("cache", expire_after=30)
 strainer = SoupStrainer("a", href=True)
 
 # If there is no such folder, the script will create one automatically
@@ -20,12 +20,13 @@ if not os.path.exists(folder_location):
 
 with open('publication_links.csv', 'r') as f:
     publication_links = f.readlines()
-    for link in publication_links:
-        response = session.get(urljoin(url, link))
+    for link in sorted(set(publication_links)):
+        response = requests.get(urljoin(url, link))
         soup = BeautifulSoup(response.text, "html.parser")
         for link in soup.select("a[href$='.pdf']"):
-            #Name the pdf files using the last portion of each link which are unique in this case
+            # Name the pdf files using the last portion of each link which are unique in this case
             filename = os.path.join(folder_location, link['href'].split('/')[-1])
+            filename = filename.replace("%20", "_")
             logger.info(f"Downloading {filename}")
             with open(filename, 'wb') as f:
                 f.write(requests.get(urljoin(url, link['href'])).content)
